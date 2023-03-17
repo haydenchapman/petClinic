f1a23c40b24a93f49c44a4dc8f2283ef786db653
haydenchapman <hayden@haydenchapman.com>
2023-03-17 06:38:06 -0500

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/com/haydenC/PetClinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/haydenC/PetClinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.haydenC.PetClinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/haydenC/PetClinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/haydenC/PetClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.haydenC.PetClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
