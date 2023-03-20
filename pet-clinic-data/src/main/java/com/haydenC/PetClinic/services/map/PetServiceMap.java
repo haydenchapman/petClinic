7d20a7243998e148219b38907bc52df3dc3184dc
haydenchapman <hayden@haydenchapman.com>
2023-03-20 11:41:34 -0500

    Impl Map Based Services, closes #4

---
+++ pet-clinic-data/src/main/java/com/haydenC/PetClinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.haydenC.PetClinic.services.map;
+
+import com.haydenC.PetClinic.model.Pet;
+import com.haydenC.PetClinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long>{
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return this.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
