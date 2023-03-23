30d5776fc8976058183ccc919bf57c08ea14d3db
haydenchapman <hayden@haydenchapman.com>
2023-03-23 11:38:26 -0500

    Added Owners List and updated controller. closes #15

--- pet-clinic-web/src/main/java/com/haydenC/PetClinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/haydenC/PetClinic/bootstrap/DataLoader.java
@@ -29,27 +29,25 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
         ownerService.save(owner2);
+
         System.out.println("Loaded owners.....");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-
         vetService.save(vet2);
+
         System.out.println("Loaded vets.....!");
 
     }
